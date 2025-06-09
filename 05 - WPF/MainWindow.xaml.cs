77ad70f20ae625bad9c63c295e5e50ffdbfe9ee1
Desai0 <kolobokevgenij83@gmail.com>
2025-06-09 14:53:27 +0300

    awdawd

---
+++ 05 - WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+// Это code-behind
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textbox1.Text))
+            {
+                listbox1.Items.Add(textbox1.Text);
+                textbox1.Clear();
+            }
+        }
+
+    }
+}
